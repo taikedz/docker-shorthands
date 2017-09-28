a1ae02b0d0c18a0fa1f7ffa9125dee8d55be576b
Tai Kedzierski <dch.tai@gmail.com>
2017-09-28 10:43:46 +0100

    check for pre-set symlink

--- install.sh
+++ install.sh
@@ -11,4 +11,9 @@ DOCKS_LIBDIR="/usr/share/docker-shorthands"
 
 mkdir -p "$DOCKS_LIBDIR"
 cp -r python/docks python/dockshort "$DOCKS_LIBDIR/"
-ln -s "$DOCKS_LIBDIR/docks" /usr/bin/docks
+
+if [ ! -e "$DOCKS_LIBDIR/docks" ]; then
+	ln -s "$DOCKS_LIBDIR/docks" /usr/bin/docks
+else
+	ls -l "/usr/bin/docks"
+fi
